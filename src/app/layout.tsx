488de929290e5381838748002390788e5dc81850
akshatsri47 <ak.sr2201@gmail.com>
2026-02-04 14:34:13 +0530

    tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import "./globals.css";
 import { Metadata } from "next";
+import Script from "next/script";
 import { generateOrganizationStructuredData } from "@/lib/seo";
 import StructuredDataComponent from "@/components/StructuredData";
 import Navbar from "@/components/Navbar";
@@ -82,8 +83,25 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#4f8e42" />
         <link rel="manifest" href="/manifest.json" />
         <StructuredDataComponent data={generateOrganizationStructuredData()} />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KD7NLSC8');`}
+        </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KD7NLSC8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Navbar />
         <CouponProvider>
           <GoogleTranslateScript />
